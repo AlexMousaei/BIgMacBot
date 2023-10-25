9da392338090f84cea7e3b85edc2b267c28c7a13
elijah <elijah.l.mayhew@student.uts.edu.au>
2023-10-25 21:45:06 +1100

    LinearUR3 Joint limits updates - Elijah

--- LinearUR3/LinearUR3.m
+++ LinearUR3/LinearUR3.m
@@ -28,7 +28,7 @@ classdef LinearUR3 < RobotBaseClass
             % dist = 0.0398+0.08654;
             link(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link
             % link(1) = Link([pi     0       0       0    1]); % PRISMATIC Link
-            link(2) = Link('d',0.1519,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0.1519,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]), 'offset',0);
             link(3) = Link('d',0,'a',-0.24365,'alpha',0,'qlim', deg2rad([-90 90]), 'offset',-pi/2);
             link(4) = Link('d',0,'a',-0.21325,'alpha',0,'qlim', deg2rad([-170 170]), 'offset', 0);
             link(5) = Link('d',0.11235,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', -pi/2);
@@ -37,8 +37,13 @@ classdef LinearUR3 < RobotBaseClass
             %link(8) = Link('d',0.1500,'a',0,   'alpha',0,   'offset',0,'qlim',deg2rad([-360,360]));
 
             % Incorporate joint limits
-            link(1).qlim = [-0.81 -0.01];
-            % link(1).qlim = [0.01 0.81];
+            link(1).qlim = [-0.8 -0.01];
+            link(2).qlim = [-180 180]*pi/180;
+            link(3).qlim = [-90 90]*pi/180;
+            link(4).qlim = [-170 170]*pi/180;
+            link(5).qlim = [-360 360]*pi/180;
+            link(6).qlim = [-360 360]*pi/180;
+            link(7).qlim = [-360 360]*pi/180;
             
             % Create robot
             self.model = SerialLink(link,'name',self.name);
