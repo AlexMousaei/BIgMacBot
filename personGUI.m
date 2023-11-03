96aa61a5f5fe7dd8e3dc3dfad81ad59c861b3bcd
AlexMousaei <99868343+AlexMousaei@users.noreply.github.com>
2023-11-03 15:40:46 +1100

    delete

--- personGUI.m
+++
@@ -1,9 +0,0 @@
-function personGUI()
-   
-    % Create figure and button
-    f = figure('Name', 'personGUI', 'NumberTitle', 'off', 'Position', [100, 100, 200, 100]);
-    btnHandle = uicontrol('Style', 'pushbutton', 'String', 'Press to run', ...
-                    'Position', [100, 50, 200, 100], ...
-                    'Callback', @toggleStop, ...
-                    'Tag', 'person');
-end
